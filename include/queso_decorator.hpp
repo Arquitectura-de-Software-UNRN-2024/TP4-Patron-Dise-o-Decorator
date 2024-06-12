fc32ad44caa7da1b9471def4c0a7632b95bbf6a9
LariTor <lara.a.torletti@gmail.com>
2024-06-12 02:20:53 -0300

    todos los decorator listoss

---
+++ include/queso_decorator.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file queso_decorator.hpp
+ * @author Pojmaevich Mirko (mirkopoj@gmail.com)
+ *         Torletti Lara (lara.a.torletti@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-12
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#pragma once
+
+#include "aditional_decorator.hpp"
+
+class QuesoDecorator : public AditionalDecorator {
+public:
+    QuesoDecorator(Combo &decorado);
+
+    virtual std::string getDescripcion() override;
+    virtual float getValor() override;
+};
