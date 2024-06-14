520facc05b21da462451baf1b2527bfde9497d31
LariTor <lara.a.torletti@gmail.com>
2024-06-14 00:25:24 -0300

    fun file en hpp y cpp divididos

--- include/func.hpp
+++ include/func.hpp
@@ -1,98 +1,35 @@
+/**
+ * @file func.hpp
+ * @author Pojmaevich Mirko (mirkopoj@gmail.com)
+ *         Torletti Lara (lara.a.torletti@gmail.com)
+ * @brief menu maker
+ * @version 0.1
+ * @date 2024-06-13
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
 #pragma once
 #include <termios.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-static struct termios old, newa;
+void gotoxy(int x, int y);
 
-void gotoxy(int x, int y)
-{
-    printf("\033[%d;%dH", x, y);
-}
+void initTermios(int echo);
 
-void initTermios(int echo)
-{
-    tcgetattr(0, &old);
-    newa = old;
-    newa.c_lflag &= ~ICANON;
-    newa.c_lflag &= echo ? ECHO : ~ECHO;
-    tcsetattr(0, TCSANOW, &newa);
-}
+void resetTermios(void);
 
-void resetTermios(void)
-{
-    tcsetattr(0, TCSANOW, &old);
-}
+bool kbhit();
 
-bool kbhit()
-{
-    termios term;
-    tcgetattr(0, &term);
+char getch_(int echo);
 
-    termios term2 = term;
-    term2.c_lflag &= ~ICANON;
-    tcsetattr(0, TCSANOW, &term2);
+char getch(void);
 
-    int byteswaiting;
-    ioctl(0, FIONREAD, &byteswaiting);
+char getche(void);
 
-    tcsetattr(0, TCSANOW, &term);
+int getmax_x();
 
-    return byteswaiting > 0;
-}
-
-char getch_(int echo)
-{
-    char ch;
-    initTermios(echo);
-    ch = getchar();
-    resetTermios();
-    return ch;
-}
-
-char getch(void)
-{
-    return getch_(0);
-}
-
-char getche(void)
-{
-    return getch_(1);
-}
-
-int getmax_x()
-{
-    int lines = 24;
-    #ifdef TIOCGSIZE
-        struct ttysize ts;
-        ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
-
-        lines = ts.ts_lines;
-    #elif defined(TIOCGWINSZ)
-        struct winsize ts;
-        ioctl ( STDIN_FILENO, TIOCGWINSZ, &ts);
-
-        lines = ts.ws_row;
-    #endif /* TIOCGSIZE */
-        return lines;
-}
-
-int getmax_y()
-{
-    int cols = 80;
-
-    #ifdef TIOCGSIZE
-        struct ttysize ts;
-        ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
-        cols = ts.ts_cols;
-
-    #elif defined(TIOCGWINSZ)
-        struct winsize ts;
-        ioctl ( STDIN_FILENO, TIOCGWINSZ, &ts);
-        cols = ts.ws_col;
-
-    #endif /* TIOCGSIZE */
-    return cols;
-}
-
+int getmax_y();
